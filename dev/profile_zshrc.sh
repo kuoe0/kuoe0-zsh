dbc9f886c7bd83cae1edee9be1f64a58cbb4c063
KuoE0 <kuoe0.tw@gmail.com>
2016-04-25 15:25:39 +0800

    Leave only the part of digits in output of time

--- dev/profile_zshrc.sh
+++ dev/profile_zshrc.sh
@@ -5,23 +5,20 @@
 #
 # Distributed under terms of the MIT license.
 #
+
+ITERATION=${1:-10}
+
 SED_COMMAND=sed
 if [ "$(uname)" = "Darwin" ]; then
 	SED_COMMAND=gsed
 fi
 
-TOTAL=0
-ITERATION=10
-
+TOTAL_TIME=0
 for t in $(seq $ITERATION); do
-	EXEC_TIME=$(/usr/bin/time -p zsh -i -c exit 2>&1 | \
-				grep real | \
-				$SED_COMMAND -e 's/real[[:space:]]\+//')
+	EXEC_TIME=$(/usr/bin/time -p zsh -i -c exit 2>&1 | grep real | grep -o -e '[[:digit:]]\+\.[[:digit:]]\+')
 	echo "Test $t: $EXEC_TIME sec"
-	TOTAL=$(echo "$TOTAL + $EXEC_TIME" | bc)
+	TOTAL_TIME=$(echo "$TOTAL_TIME + $EXEC_TIME" | bc -l)
 done
 
-AVERAGE=$(echo "$TOTAL / $ITERATION" | \
-          bc -l | \
-          awk '{printf("%.2f", $1 + 0.005)}')
-echo "Average: $(echo "$TOTAL / $ITERATION" | bc -l | awk '{printf("%.2f", $1 + 0.005)}') sec"
+AVERAGE_TIME=$(echo "$TOTAL_TIME / $ITERATION" | bc -l | awk '{printf("%.2f", $1 + 0.005)}')
+echo "Average: $AVERAGE_TIME sec"
