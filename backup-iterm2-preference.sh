fd0c2b5129100ede0cc15e69df746078fe606246
KuoE0 <kuoe0.tw@gmail.com>
2017-11-14 18:00:53 +0800

    add script to backup iterm2 preference

---
+++ backup-iterm2-preference.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# backup-iterm2-preference.sh
+# KuoE0 <kuoe0.tw@gmail.com>
+# Copyright (C) 2017
+#
+# Distributed under terms of the MIT license.
+#
+
+cp $HOME/Library/Preferences/com.googlecode.iterm2.plist $PWD
