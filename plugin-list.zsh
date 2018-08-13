c615fd860e9562684fda572d34c69cd4d2003fad
chkuo <chkuo@google.com>
2018-08-13 17:53:54 +0800

    add plugin: docker-zsh-completion

--- plugin-list.zsh
+++ plugin-list.zsh
@@ -3,6 +3,7 @@ zplug "bric3/nice-exit-code"
 zplug "chriskempson/base16-shell", use:"scripts/base16-tomorrow-night.sh" # base16 tomorrow color support
 zplug "chrissicool/zsh-256color"
 zplug "djui/alias-tips" # remind me of my alias
+zplug "felixr/docker-zsh-completion"
 zplug "mafredri/zsh-async" # perform task asynchronously (needed by pure theme)
 zplug "modules/archive", from:prezto
 zplug "modules/command-not-found", from:prezto, if:"[ \"$(uname -s)\" = \"Linux\" ]"
@@ -22,7 +23,6 @@ zplug "modules/python", from:prezto
 zplug "modules/rsync", from:prezto
 zplug "modules/ruby", from:prezto
 zplug "modules/spectrum", from:prezto
-zplug "zpm-zsh/linuxbrew", if:"[ \"$(uname -s)\" = \"Linux\" ]"
 zplug "modules/tmux", from:prezto
 zplug "modules/utility", from:prezto, defer:1 # need to be loaded after gnu-utility
 zplug "sindresorhus/pure", use:"pure.zsh", as:theme
@@ -30,6 +30,7 @@ zplug "tarrasch/zsh-autoenv"
 zplug "unixorn/git-extra-commands" # a collection of useful extra git scripts
 zplug "unixorn/warhol.plugin.zsh"
 zplug "zdict/zdict.sh" # zdict completion scripts
+zplug "zpm-zsh/linuxbrew", if:"[ \"$(uname -s)\" = \"Linux\" ]"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search", defer:2 # avoid the setting overwritten by zsh-syntax-highlighting
 zplug "zsh-users/zsh-syntax-highlighting", defer:1 # avoid the setting overwritten by zsh-autosuggestions
